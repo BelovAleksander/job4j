6f59fe8f30cd85cdfeed1b286c84ba118a271102
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-05-24 00:17:20 +0700

    1.3. JavaDoc. Code convention [ #4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,4 +1,11 @@
-package main.java.ru.job4j.calculate;
+package ru.job4j.calculate;
+
+/**
+ * Class Класс для вывода "Hello World" в консоль
+ * @author abelov
+ * @since 24.05.2018
+ * @version 1
+ */
 
 public class Calculate {
     public static void main(String[] args) {
