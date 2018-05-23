91120b5c52c719a14cbac301aabcbfed3eab10ad
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-05-24 02:28:37 +0700

    1.4. JUnit [ #4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,14 +1,27 @@
 package ru.job4j.calculate;
 
 /**
- * 
+ * Calculate.
+ *
  * @author abelov
- * @since 24.05.2018
- * @version 1
+ * @version $Id$
+ * @since 0.1
  */
 
 public class Calculate {
+    /** 
+    * Main.
+    * @param args - args.
+    */
     public static void main(String[] args) {
 	System.out.println("Hello World");
+    }
+    /**
+    * Method echo.
+    * @param name Your name.
+    * @return Echo plus Your name.
+    */
+    public String echo(String name) {
+	return "Echo, echo, echo : " + name;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for test task.
+*
+* @author Alexander Belov
+* @version 1
+* @since 24.05.2018
+*/
+
+package ru.job4j.calculate;
