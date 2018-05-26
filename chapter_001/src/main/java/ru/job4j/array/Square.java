3bbbfbb4add426c796a121347680a38901fa83de
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-05-27 05:58:57 +0700

    5.0. Заполнить массив степенями чисел. [ #33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ * @author whiterabbit.nsk
+ * version $Id$
+ * since 0.1
+ */
+public class Square {
+    /**
+     * Метод создает последовательность чисел, возведенных в квалрат
+     * @param bound количество чисел в массиве
+     * @return массив
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        int i = 0;
+        for (int num = 1; num <= bound; num++) {
+            rst[i] = (int)Math.pow(num, 2);
+            i++;
+        }
+        return rst;
+    }
+}
