adf84b40fcffe2e9a835e83992b3e3a0f40dd110
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-05-29 03:07:57 +0700

    5.3. Массив заполнен true или false [ #53857]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * @author Alexander Belov (whiterabbit.nsk@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Check {
+    /**
+     * Проверяет массив на идентичность значений.
+     * @param data входящий массив.
+     * @return true, если значения идентичны.
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != data[0]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
