5f325662374499ed9ff01c0d726636ab5b80411f
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-06-19 00:01:20 +0700

    Распарсить скобки. [#34113]

---
+++ chapter_003/src/main/java/ru/job4j/tests/Parsing.java
@@ -0,0 +1,51 @@
+package ru.job4j.tests;
+
+/**
+ * Проверяет входящую строку на разницу
+ * числа открывающих и закрывающих скобок.
+ * Пока не придумал как отследить такие
+ * неверные входящие значения:
+ * "{([)]}"
+ * @author Alexander Belov (whiterabbit.nsk@gmail.com)
+ * @since 18.06.18
+ */
+
+public class Parsing {
+    public boolean isValide(String string) {
+        boolean result = true;
+        if ((string.startsWith("}")) || (string.startsWith("]")) || (string.startsWith(")"))) {
+            result = false;
+        } else {
+            char[] array = string.toCharArray();
+            StringBuilder openBracket = new StringBuilder();
+            StringBuilder closeBracket = new StringBuilder();
+            StringBuilder openFigure = new StringBuilder();
+            StringBuilder closeFigure = new StringBuilder();
+            StringBuilder openSquare = new StringBuilder();
+            StringBuilder closeSquare = new StringBuilder();
+            for (char bracket : array) {
+                if (bracket == 123) {
+                    openFigure.append(bracket);
+                } else if (bracket == 125) {
+                    closeFigure.append(bracket);
+                } else if (bracket == 40) {
+                    openBracket.append(bracket);
+                } else if (bracket == 41) {
+                    closeBracket.append(bracket);
+                } else if (bracket == 91) {
+                    openSquare.append(bracket);
+                } else if (bracket == 93) {
+                    closeSquare.append(bracket);
+                } else {
+                    result = false; // Если не допустимо ничего кроме скобок.
+                }
+            }
+            if ((openBracket.length() != closeBracket.length())
+                    || (openFigure.length() != closeFigure.length())
+                    || (openSquare.length() != closeSquare.length())) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/tests/ParsingTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ParsingTest {
+    @Test
+    public void whenStartWithIllegalThenFalse() {
+        Parsing test = new Parsing();
+        boolean result = test.isValide("}{[]()");
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenSimpleOkThenTrue() {
+        Parsing test = new Parsing();
+        boolean result = test.isValide("{}[]()");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenComplexOkThenTrue() {
+        Parsing test = new Parsing();
+        boolean result = test.isValide("{{[()]}}");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDifferentAmountOfOpenAndCloseBracketThenFalse() {
+        Parsing test = new Parsing();
+        boolean result = test.isValide("{{}()]}");
+        assertThat(result, is(false));
+    }
+}
