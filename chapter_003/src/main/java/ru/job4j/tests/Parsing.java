c6ff96d50c9d0a297dced35b0dc90257e8106fa3
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-06-27 01:35:23 +0700

    Распарсить скобки. [#34113]

--- chapter_003/src/main/java/ru/job4j/tests/Parsing.java
+++ chapter_003/src/main/java/ru/job4j/tests/Parsing.java
@@ -1,70 +1,39 @@
 package ru.job4j.tests;
 
 import javax.xml.stream.events.Characters;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
- * Проверяет входящую строку на разницу
- * числа открывающих и закрывающих скобок.
+ * Проверяет входящую строку на соответствие
+ * открывающих и закрывающих скобок.
  *
  * @author Alexander Belov (whiterabbit.nsk@gmail.com)
- * @since 21.06.18
+ * @since 27.06.18
  */
 
 public class Parsing {
     public boolean isValide(String string) {
         boolean result = true;
-        if ((string.length() == 1 || string.startsWith("}"))
-                || (string.startsWith("]")) || (string.startsWith(")"))) {
-            result = false;
-        } else {
-            char bracketStart = '\0';
-            char bracketEnd = '\0';
-            int flagStart = -1;
-            int flagEnd = -1;
-            int nesting = 0; // вложенность
-            char[] array = string.toCharArray();
-            int openBrackets = 0;
-            int closeBrackets = 0;
-            for (int index = 0; index < array.length; index++) {
-                if (array[index] == bracketStart) {
-                    nesting++;
-                } else if ((nesting == 0 && array[index] == 123)
-                        || (nesting == 0 && array[index] == 91)
-                        || (nesting == 0 && array[index] == 40)) {
-                    bracketStart = array[index];
-                    bracketEnd = bracketStart == 40 ? ')' : (char) (bracketStart + 2);
-                    flagStart = index;
-                    nesting++;
-                } else if (array[index] == bracketEnd) {
-                    if (nesting == 1) {
-                        flagEnd = index;
-                        result = isValide(
-                                String.copyValueOf(
-                                        array, flagStart + 1, flagEnd - flagStart - 1));
-                        if (!result) {
-                            break;
-                        }
-                        bracketStart = '\0';
-                        bracketEnd = '\0';
-                    }
-                    nesting--;
-                }
-            }
-            for (char bracket : array) {
-                if (bracket == 123 || bracket == 40 || bracket == 91) {
-                    openBrackets++;
-                } else if (bracket == 125 || bracket == 41 || bracket == 93) {
-                    closeBrackets++;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            } else if (ch == '}' || ch == ']' || ch == ')') {
+                if (stack.empty()) {
+                    System.out.println(String.format("Bracket %s doesn't have pair!", ch));
+                    result = false;
+                    break;
                 } else {
-                    result = false; // Если не допустимо ничего кроме скобок.
+                    char chx = stack.pop();
+                    if ((ch == '}' && chx != '{')
+                            || (ch == ']' && chx != '[')
+                            || (ch == ')' && chx != '(')) {
+                        System.out.println(String.format("Bracket %s not equals %s!", ch, chx));
+                        result = false;
+                        break;
+                    }
                 }
-            }
-            if (openBrackets != closeBrackets) {
-                result = false;
             }
         }
         return result;
