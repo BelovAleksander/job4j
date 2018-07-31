a38c5c4172f38c9b911780bb6287c486aeaf55be
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-07-31 14:58:29 +0700

    Исправления(1. UML системы [ #1093])

--- chapter_006/src/main/java/ru/job4j/create.sql
+++ chapter_006/src/main/java/ru/job4j/create.sql
@@ -1,54 +1,57 @@
 
-CREATE DATABASE system_of_items;
 
 CREATE TABLE roles (
-title text PRIMARY KEY
+id serial PRIMARY KEY,
+title text
 );
 
 CREATE TABLE rules (
-title text PRIMARY KEY
+id serial PRIMARY KEY,
+title text
 );
 
 CREATE TABLE users (
-user_id serial PRIMARY KEY,
+id serial PRIMARY KEY,
 name text,
-role text REFERENCES roles (title)
+role_id serial REFERENCES roles (id)
 );
 
 CREATE TABLE rules_of_roles (
-role text REFERENCES roles (title),
-rule text REFERENCES rules (title),
+role_id serial REFERENCES roles (id),
+rule_id serial REFERENCES rules (id),
 is_active boolean,
-CONSTRAINT id PRIMARY KEY (role, rule)
+CONSTRAINT id PRIMARY KEY (role_id, rule_id)
 );
 
 CREATE TABLE categories (
-title text PRIMARY KEY
+id serial PRIMARY KEY,
+title text
 );
 
 CREATE TABLE states (
-title text PRIMARY KEY
+id serial PRIMARY KEY,
+title text
 );
 
 CREATE TABLE items (
-item_id serial PRIMARY KEY,
+id serial PRIMARY KEY,
 description text,
-user_id serial REFERENCES users (user_id),
-category text REFERENCES categories (title),
-state text REFERENCES states (title)
+user_id serial REFERENCES users (id),
+cat_id serial REFERENCES categories (id),
+st_id serial REFERENCES states (id)
 );
 
 CREATE TABLE attaches (
-att_id serial PRIMARY KEY,
+id serial PRIMARY KEY,
 content text,
-item serial REFERENCES items (item_id)
+item_id serial REFERENCES items (id)
 );
 
 CREATE TABLE comments (
-com_id serial PRIMARY KEY,
+id serial PRIMARY KEY,
 content text,
-item serial REFERENCES items (item_id),
-user_id serial REFERENCES users (user_id)
+item serial REFERENCES items (id),
+user_id serial REFERENCES users (id)
 );
 
 
@@ -61,16 +64,16 @@ VALUES ('can create new item'),
 	('can remove item'),
 	('can block user');
 
-INSERT INTO rules_of_roles (role, rule, is_active)
-VALUES ('admin', 'can create new item', true),
-	('admin', 'can remove item', true),
-	('admin', 'can block user', true),
-	('user', 'can create new item', true);
+INSERT INTO rules_of_roles (role_id, rule_id, is_active)
+VALUES (1, 1, true),
+	(1, 2, true),
+	(1, 3, true),
+	(2, 1, true);
 
-INSERT INTO users (user_id, name, role)
-VALUES (1, 'Alex', 'user'),
-	(2, 'Kate', 'user'),
-	(777, 'Veniamin', 'admin');
+INSERT INTO users (id, name, role_id)
+VALUES (1, 'Alex', 2),
+	(2, 'Kate', 2),
+	(777, 'Veniamin', 1);
 
 INSERT INTO categories (title)
 VALUES ('offers'),
@@ -81,15 +84,14 @@ VALUES ('is active'),
 	('completed'),
 	('removed');
 
-INSERT INTO items (item_id, description, user_id, category, state)
-VALUES (1, 'Lets take a party!', 1, 'offers', 'is active'),
-	(2, 'I have a small salary', 2, 'complaints', 'removed'),
-	(3, 'Staff reduction is required', 777, 'offers', 'completed');
+INSERT INTO items (id, description, user_id, cat_id, st_id)
+VALUES (1, 'Lets take a party!', 1, 1, 1),
+	(2, 'I have a small salary', 2, 2, 3),
+	(3, 'Staff reduction is required', 777, 1, 2);
 
-INSERT INTO attaches (att_id, content, item)
+INSERT INTO attaches (id, content, item_id)
 VALUES (12, 'some beer...', 1),
 	(32, 'declaration', 3);
 
-INSERT INTO comments (com_id, content, item, user_id)
+INSERT INTO comments (id, content, item, user_id)
 VALUES (12, 'OK', 1, 2);
-
