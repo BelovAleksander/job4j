fd46a1778a5add0d8a6fa85e7af0d93c3c66ae8e
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-08-06 21:41:33 +0700

    Парсер вакансий на sql.ru [ #1731]

--- chapter_007/src/main/java/ru/job4j/parser/ParseJob.java
+++ chapter_007/src/main/java/ru/job4j/parser/ParseJob.java
@@ -19,75 +19,68 @@ import java.util.ResourceBundle;
 
 public class ParseJob implements Job {
     private static final Logger LOG = Logger.getLogger(ParseJob.class);
-    private Connection conn = null;
     private boolean haveBase = false;
     private boolean firstExecute = true;
     private String config;
+    private String url;
+    private String user;
+    private String password;
 
     public ParseJob(String config) {
         this.config = config.substring(0, config.indexOf('.'));
     }
 
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    public void execute(JobExecutionContext jobExecutionContext) {
         try {
-            init();
+            initConnection();
             parse();
-        } catch (SQLException e) {
-            LOG.error("SQL Exception");
-            e.printStackTrace();
-        } catch (ParseException e) {
-            LOG.error("Parse Exception");
-            e.printStackTrace();
-        } catch (IOException e) {
-            LOG.error("IO Exception");
+        } catch (Exception e) {
+            LOG.error("Exception");
             e.printStackTrace();
         }
     }
 
-    private void init() throws SQLException {
+    private void checkDB(String user, String password) throws SQLException {
+        String url = String.format("jdbc:postgresql://localhost/?user=%s&password=%s", user, password);
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE datname = 'vacancies_db');";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            haveBase = rs.getBoolean(1);
+            rs.close();
+            if (!haveBase) {
+                LOG.info("DB doesn't exist. Creating new DB...");
+                st.executeUpdate("CREATE database vacancies_db;");
+                haveBase = true;
+            } else {
+                LOG.info("DB exist. FirstExecute = false");
+                firstExecute = false;
+            }
+        }
+    }
+
+    private void initConnection() throws SQLException {
         LOG.info("Start init() method...");
         ResourceBundle resource = ResourceBundle.getBundle(config);
         String driver = resource.getString("driver");
-        String user = resource.getString("user");
-        String password = resource.getString("password");
-        String url = resource.getString("url");
-        String urlForFirstExecute = String.format("jdbc:postgresql://localhost/?user=%s&password=%s", user, password);
+        this.user = resource.getString("user");
+        this.password = resource.getString("password");
+        this.url = resource.getString("url");
 
-        conn = DriverManager.getConnection(urlForFirstExecute);
-        String sql = "SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE datname = 'vacancies_db');";
-        Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery(sql);
-        rs.next();
-        haveBase = rs.getBoolean(1);
-        rs.close();
-
-        if (!haveBase) {
-            LOG.info("DB doesn't exist. Creating new DB...");
-            sql = "CREATE database vacancies_db;";
-            st.executeUpdate(sql);
-            haveBase = true;
-        } else {
-            LOG.info("DB exist. firstExecute = false");
-            firstExecute = false;
-        }
+        checkDB(user, password);
         try {
             Class.forName(driver).newInstance();
-        } catch (InstantiationException e) {
-            LOG.error("Instantiation Exception");
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            LOG.error("Illegal Access Exception");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            LOG.error("Class Not Found");
+        } catch (Exception e) {
+            LOG.error("Exception");
             e.printStackTrace();
         }
-        conn = DriverManager.getConnection(url, user, password);
-        st = conn.createStatement();
-        sql = "CREATE TABLE IF NOT EXISTS vacancies(id serial PRIMARY KEY, description text, create_date date, UNIQUE(description, create_date))";
-        st.executeUpdate(sql);
-        st.close();
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement st = conn.createStatement()) {
+            st.executeUpdate("CREATE TABLE IF NOT EXISTS vacancies(id serial PRIMARY KEY, description text,"
+                    + " create_date date, UNIQUE(description, create_date))");
+        }
         LOG.info("Ending init() method...");
     }
 
@@ -102,11 +95,52 @@ public class ParseJob implements Job {
             doc = Jsoup.connect("http://www.sql.ru/forum/actualsearch.aspx?search="
                     + "java+%F0%E0%E7%F0%E0%E1%EE%F2%F7%E8%EA&sin=1&st=t&bid=0&a=&ma=0&dt=1&s=1&so=1").get();
         }
-        Elements descsEl = doc.select("td.postslisttopic").select("a:contains(java)");
+        Elements vacanciesEl = doc.select("td.postslisttopic").select("a:contains(java)");
         Elements datesEl = doc.select("td.altCol").select("[style=text-align:center]");
-        String[] descs = convert(descsEl);
-        String[] dates = convert(datesEl);
+        String[] vacancies = clip(vacanciesEl);
+        String[] datesStr = clip(datesEl);
+        Date[] dates = formatDate(datesStr);
+
+        for (int i = 0; i < vacancies.length; i++) {
+            if (!vacancies[i].toLowerCase().contains("javascript")
+                    || !vacancies[i].toLowerCase().contains("java script")) {
+                paste(vacancies[i], dates[i]);
+            }
+        }
+    }
+
+    private void paste(String text, Date createDate) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            String sql = "INSERT INTO vacancies(description, create_date) VALUES(?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, text);
+            ps.setDate(2, createDate);
+            try {
+                ps.execute();
+                LOG.info("vacancy pasted");
+            } catch (PSQLException e) {
+                LOG.error("Duplicate vacancy");
+            }
+            ps.close();
+        }
+    }
+
+    private String[] clip(Elements elements) {
+        String[] array = new String[elements.size()];
+        int i = 0;
+        for (Element el : elements) {
+            String str = el.toString();
+            int start = str.indexOf('>') + 1;
+            int stop = str.lastIndexOf('<');
+            array[i] = (str.substring(start, stop));
+            i++;
+        }
+        return array;
+    }
+
+    private Date[] formatDate(String[] dates) throws ParseException {
         DateFormat dateFormat = new SimpleDateFormat("dd MMM yy");
+        Date[] newDates = new Date[dates.length];
         for (int i = 0; i < dates.length; i++) {
             dates[i] = dates[i].substring(0, dates[i].length() - 7);
             if (dates[i].contains("сегод")) {
@@ -114,43 +148,11 @@ public class ParseJob implements Job {
             } else if (dates[i].contains("вчера")) {
                 dates[i] = dateFormat.format(new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000));
             }
+            java.util.Date dateUtil = dateFormat.parse(dates[i]);
+            Date dateSql = new Date(dateUtil.getTime());
+            newDates[i] = dateSql;
         }
-        for (int i = 0; i < descs.length; i++) {
-            if (!descs[i].toLowerCase().contains("javascript")
-                    || !descs[i].toLowerCase().contains("java script")) {
-                java.util.Date dateUtil = dateFormat.parse(dates[i]);
-                Date dateSql = new Date(dateUtil.getTime());
-                paste(descs[i], dateSql);
-            }
-        }
-        conn.close();
-    }
-
-    private void paste(String text, Date createDate) throws SQLException {
-        String sql = "INSERT INTO vacancies(description, create_date) VALUES(?, ?)";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1, text);
-        ps.setDate(2, createDate);
-        try {
-            ps.execute();
-            LOG.info("vacancy pasted");
-        } catch (PSQLException e) {
-            LOG.error("Duplicate vacancy");
-        }
-        ps.close();
-    }
-
-    private String[] convert(Elements elements) {
-        String[] array = new String[elements.size()];
-        int i = 0;
-        for (Element el : elements) {
-            String string = el.toString();
-            int start = string.indexOf('>') + 1;
-            int stop = string.lastIndexOf('<');
-            array[i] = (string.substring(start, stop));
-            i++;
-        }
-        return array;
+        return newDates;
     }
 
     public static void main(String[] args) {
@@ -160,7 +162,7 @@ public class ParseJob implements Job {
         }
         ResourceBundle resource = ResourceBundle.getBundle("app");
         String cron = resource.getString("cron_expression");
-        HashMap<String, String> map = new HashMap();
+        HashMap<String, String> map = new HashMap<>();
         map.put("Data#1", config);
         JobDetail parseJob = JobBuilder.newJob(ParseJob.class).withIdentity("parseJob", "group1")
                 .setJobData(new JobDataMap(map)).build();
