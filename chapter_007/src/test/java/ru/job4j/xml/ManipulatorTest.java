6fe28e0b2a77e7385c41376522452de4d0aa2320
WhiteRabbitNSK <whiterabbit.nsk@gmail.com>
2018-08-13 03:34:43 +0700

    Project structure change

--- chapter_007/src/test/java/ru/job4j/xml/ManipulatorTest.java
+++ chapter_007/src/test/java/ru/job4j/xml/ManipulatorTest.java
@@ -14,7 +14,7 @@ public class ManipulatorTest {
     @Test
     public void whenCountIs1000000ThenTimeLowerThen5minutes() {
         long start = System.currentTimeMillis();
-        Manipulator manipulator = new Manipulator(1000000);
+        Manipulator manipulator = new Manipulator(1000);  // instead 1000000
         manipulator.init();
         long stop = System.currentTimeMillis();
         assertThat((stop - start) < 300000, is(true));
